f7caacecd6f907067b3fa82df56b3e81998744e2
Allan Roscoche <allanroscoche@gmail.com>
2020-02-05 08:29:30 -0300

    Added login sample page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,25 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+    display: flex;
+    background-color: red
+    flex-direction: column;
+`;
+
 function Home() {
-    return <div>Ola</div>
+    return (
+        <Card>
+            <div>
+                email: <input />
+            </div>
+            <div>
+                senha: <input type="password" />
+            </div>
+            <div>
+                <input type="submit" /> 
+            </div>
+
+        </Card>
+    );
 }
 export default Home;
